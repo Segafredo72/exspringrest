6afa7856d8aff64d4972d26f895323f59c362689
Siegfried Smeets <siegfried.smeets@bnpparibasfortis.com>
2020-10-15 14:32:13 +0200

    ExB1 JUNIT test fixed & working

--- ExB1/src/test/java/be/abis/exercise/ut/TestPersonService.java
+++ ExB1/src/test/java/be/abis/exercise/ut/TestPersonService.java
@@ -3,18 +3,22 @@ package be.abis.exercise.ut;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import be.abis.exercise.service.ApiPersonService;
-import be.abis.exercise.service.PersonService;
 
+@SpringBootTest 
+@RunWith(SpringRunner.class)
 public class TestPersonService {
 
 	@Autowired
 	ApiPersonService aps;
 	
 	@Test
-	void firstPersonFirstNameJohn() {
+	public void firstPersonFirstNameJohn() {
 		int id = 1;
 		assertEquals("John", aps.findPerson(id).getFirstName());
 				
