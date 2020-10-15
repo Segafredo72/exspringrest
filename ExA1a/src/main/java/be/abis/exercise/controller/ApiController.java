f2f1bd0a8e446008048167bff1cf25d4348fbc68
Siegfried Smeets <siegfried.smeets@bnpparibasfortis.com>
2020-10-15 10:08:03 +0200

    ExA1 chg pwd added

--- ExA1a/src/main/java/be/abis/exercise/controller/ApiController.java
+++ ExA1a/src/main/java/be/abis/exercise/controller/ApiController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.abis.exercise.exception.PersonCanNotBeDeletedException;
 import be.abis.exercise.model.Login;
+import be.abis.exercise.model.Password;
 import be.abis.exercise.model.Person;
 import be.abis.exercise.service.PersonService;
 
@@ -52,6 +54,11 @@ public class ApiController {
 	@DeleteMapping("{id}")
 	public void removePersonById(@PathVariable("id") int id) throws PersonCanNotBeDeletedException {
 	personService.deletePerson(id);	
+	}
+	
+	@PutMapping(path = "{id}/password")
+	public void changePasswordById(@PathVariable("id") int id, @RequestBody Password password) throws IOException {
+		personService.changePassword(personService.findPerson(id), password.getPassword());
 	}
 	
 }
